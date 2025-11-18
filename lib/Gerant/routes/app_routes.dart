a30d71f8fe373b9ad4f321543eb1fac2c158b062
Taha Bchir <tahabchir6@gmail.com>
2025-11-19 00:57:43 +0100

    cart

--- lib/Gerant/routes/app_routes.dart
+++ lib/Gerant/routes/app_routes.dart
@@ -2,14 +2,19 @@ import 'package:flutter/material.dart';
 import '../pages/auth/signin_page.dart';
 import '../pages/auth/signup_page.dart';
 import '../home/Home.dart';
+import '../pages/cart.dart';
+
 
 class AppRoutes {
   static const String signIn = '/signin';
   static const String signUp = '/signup';
   static const String home = '/home';
+  static const String cart = '/cart';
+
 
   static Map<String, WidgetBuilder> routes = {
     signIn: (context) => const SignInScreen(),
     signUp: (context) => const SignUpScreen(),
+    cart: (context) => const CartScreen(),
   };
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bestmlewi/main.dart';
+import 'package:supabase_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
