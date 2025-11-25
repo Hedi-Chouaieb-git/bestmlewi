7b395385fd5e8402a8a1b3a78a309e4c243c7055
DhouhaAyedi <dhouhaayedi04@gmail.com>
2025-11-25 21:33:57 +0100

    client

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-import 'Gerant/routes/app_routes.dart';
+import 'Client/routes/client_routes.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://winrzmxneayivpsimfnn.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpbnJ6bXhuZWF5aXZwc2ltZm5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzODI5MjcsImV4cCI6MjA3ODk1ODkyN30.laGMkbHyi4KqpSzzZEZdcWZUWafNyzMP859rqpXmHz8',
+  );
+  runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,7 +23,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         brightness: Brightness.dark,
       ),
-      initialRoute: AppRoutes.signIn,
+      initialRoute: AppRoutes.menu,
       routes: AppRoutes.routes,
     );
   }
