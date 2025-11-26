3d257e8d362f1c946efbf592dca5c5b6b8417b62
Hadi chouaieb <hadichouaieb20@gmail.com>
2025-11-26 23:15:09 +0100

    fixing db and key supabase

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://winrzmxneayivpsimfnn.supabase.co',
-    anonKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpbnJ6bXhuZWF5aXZwc2ltZm5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzODI5MjcsImV4cCI6MjA3ODk1ODkyN30.laGMkbHyi4KqpSzzZEZdcWZUWafNyzMP859rqpXmHz8',
+    url: 'https://qxajdhjecopmgvbtbkpu.supabase.co',
+    anonKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4YWpkaGplY29wbWd2YnRia3B1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5NzMxMTYsImV4cCI6MjA3OTU0OTExNn0.CB06Fr7jcQPAFctPG7chV9yeF6B2GQldgFyqcrdq7Bc',
   );
 
   runApp(const MyApp());
@@ -17,7 +16,6 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
